7eba1d3a0c1a3c1a6c250e8d6c613acfb22dbfa7
Keran McKenzie <keran.mckenzie@gmail.com>
2013-04-05 08:46:36 +1100

    First step to OAuth

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ var theAPIkey = 'ew59q4vmuzss7nuyhm8t7st7'
 var theAPIsecret = 'w9Wm3f7te2njWPxbxXrt7Jpn'
 var theAPIredirect = 'http://www.keranmckenzie.com/' // a url that exists but doesn't really
 var theAPIredirect_encoded = encodeURIComponent( theAPIredirect ) // make sure 
+var oauthServer = 'https://secure.myob.com/oauth2/v1/authorize'
 
 var accessCode = ''
 // lets fetch the accessToken out of localstorage if we have it
@@ -25,9 +26,11 @@ if(localStorage.getItem("accessToken")) {
 	var accessToken = localStorage.getItem("accessToken")
 	var accessExpire = localStorage.getItem("accessExpire")
 	var refreshToken = localStorage.getItem("refreshToken")
+	var cfCredentials = localStorage.getItem("cfCredentials")
 } else {
 	var accessToken = ''
 	var refreshToken = ''
+	var cfCredentials = ''
 }
 
 
@@ -58,6 +61,7 @@ var appEngine = {
 	hideAll : function() {
 		$('#loading').css('display', 'none')
 		$('#welcome').css('display', 'none')
+		$('#main').css('display', 'none')
 	},
 
 	showLoading : function(loading_msg) {
@@ -130,8 +134,6 @@ var appEngine = {
 				var code = loc.split('?code=')
 				//console.log(code[1])
 				accessCode = code[1]
-				// close the childbrowser - by forcing it to load a local blank.html first
-				window.plugins.childBrowser.showWebPage(('blank.html', { showLocationBar: false, showNavigationBar: false, showAddress: false })
 
 				window.plugins.childBrowser.close()
 
@@ -145,7 +147,6 @@ var appEngine = {
 				//    error here is the user hit NO instead of YES
 				appEngine.showWelcome( messages.error_oauth_denied )
 				// close the childbrowser
-				window.plugins.childBrowser.showWebPage(('blank.html', { showLocationBar: false, showNavigationBar: false, showAddress: false })
 				window.plugins.childBrowser.close()
 
 			} else {
@@ -154,6 +155,81 @@ var appEngine = {
 		}
 
 	}, // secureMYOB
+
+	getAccessToken : function() {
+
+		// setup the headers for getting the AccessToken
+		$.ajaxSetup({
+          headers: {
+            'client_id': theAPIkey,
+            'client_secret': theAPIsecret,
+            'scope': 'CompanyFile',
+            'code': accessCode,
+            'redirect_uri': theAPIredirect_encoded,
+            'grant_type': 'authorization_code',
+          }
+        });
+
+        var response = appEngine.getURL(oauthServer, false)
+
+        // lets dump out the response
+        appEngine.hideAll()
+		$('#main').css('display', 'block')
+		$('#main #content').html(data)
+
+	}, // getAccessToken
+
+	getRefreshToken : function() {
+
+		// setup the headers for getting the RefreshToken
+		$.ajaxSetup({
+          headers: {
+            'client_id': theAPIkey,
+            'client_secret': theAPIsecret,
+            'refresh_token': refreshToken,
+            'grant_type': 'refresh_token',
+          }
+        });
+
+        var response = appEngine.getURL(oauthServer, false)
+
+        // lets dump out the response
+        appEngine.hideAll()
+		$('#main').css('display', 'block')
+		$('#main #content').html(data)
+	}, // getRefreshToken
+
+	// expects headers as bool, url as string
+	getURL : function(url, headers) {
+
+		// do we need the headers or not?
+		if(headers) {
+	        $.ajaxSetup({
+	          headers: {
+	            'Authorization': 'Bearer ' + accessToken,
+	            'x-myobapi-cftoken': cfCredentials,
+	            'x-myobapi-key': theAPIkey,
+	          }
+	        });
+		}
+
+        $.ajax
+        ({
+			type: "GET",
+			url: url,
+			dataType: 'json',
+			async: true,
+			success: function(data) {
+				// done, we have the data return it
+				return data
+			},
+			error: function(data) {
+				// there was an error - handle it
+				return data
+			}
+      });
+
+	}, // getURL
 
 
 } // end our engine
@@ -202,4 +278,137 @@ Storage.prototype.setObject = function(key, value) {
 Storage.prototype.getObject = function(key) {
     var value = this.getItem(key);
     return value && JSON.parse(value);
+}
+
+//
+// Ability to Base64 encode
+//
+/**
+*  Base64 encode / decode
+*  http://www.webtoolkit.info/
+**/
+
+var Base64 = { 
+  // private property
+  _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+  // public method for encoding
+  encode : function (input) {
+    var output = "";
+    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+    var i = 0;
+
+    input = Base64._utf8_encode(input);
+
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
+
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
+
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+        enc4 = 64;
+      }
+
+      output = output +
+      this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+      this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+    }
+
+    return output;
+  },
+
+  // public method for decoding
+  decode : function (input) {
+    var output = "";
+    var chr1, chr2, chr3;
+    var enc1, enc2, enc3, enc4;
+    var i = 0;
+
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++));
+      enc2 = this._keyStr.indexOf(input.charAt(i++));
+      enc3 = this._keyStr.indexOf(input.charAt(i++));
+      enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
+
+      output = output + String.fromCharCode(chr1);
+
+      if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+      }
+    }
+
+    output = Base64._utf8_decode(output);
+
+    return output;
+  },
+
+  // private method for UTF-8 encoding
+  _utf8_encode : function (string) {
+    string = string.replace(/\r\n/g,"\n");
+    var utftext = "";
+
+    for (var n = 0; n < string.length; n++) {
+      var c = string.charCodeAt(n);
+
+      if (c < 128) {
+        utftext += String.fromCharCode(c);
+      }
+      else if((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224);
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+        utftext += String.fromCharCode((c & 63) | 128);
+      }
+    }
+
+    return utftext;
+  },
+
+  // private method for UTF-8 decoding
+  _utf8_decode : function (utftext) {
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+
+    while ( i < utftext.length ) { 
+      c = utftext.charCodeAt(i);
+
+      if (c < 128) {
+        string += String.fromCharCode(c);
+        i++;
+      }
+      else if((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i+1);
+        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+        i += 2;
+      }
+      else {
+        c2 = utftext.charCodeAt(i+1);
+        c3 = utftext.charCodeAt(i+2);
+        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+        i += 3;
+      }
+    }
+
+    return string;
+  }
 }
