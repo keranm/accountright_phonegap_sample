d3dcf36258b8fb8efa004bc6ff600ff1d28f116f
Keran McKenzie <keran.mckenzie@gmail.com>
2013-04-04 21:04:07 +1100

    Sorting out messaging and oauth error handling

--- js/main.js
+++ js/main.js
@@ -19,16 +19,23 @@ var theAPIsecret = 'w9Wm3f7te2njWPxbxXrt7Jpn'
 var theAPIredirect = 'http://www.keranmckenzie.com/' // a url that exists but doesn't really
 var theAPIredirect_encoded = encodeURIComponent(theAPIredirect) // make sure 
 
+var accessCode = ''
+// lets fetch the accessToken out of localstorage if we have it
+if(localStorage.getItem("accessToken")) {
+	var accessToken = localStorage.getItem("accessToken")
+	var accessExpire = localStorage.getItem("accessExpire")
+	var refreshToken = localStorage.getItem("refreshToken")
+} else {
+	var accessToken = ''
+	var refreshToken = ''
+}
+
+
 
 // we want to ensure a tight fit on all mobiles, so lets set the sizes right
 var width = window.innerWidth
 var height = window.innerHeight // use native JS not any plugin to get the right sizes
 var loadingOffset = 1000
-
-// lets get anything out of localstorage that might (or not) be there
-var accessToken = localStorage.getItem('accessToken');
-var tokenExpiry = localStorage.getItem('tokenExpiry');
-var refreshToken = localStorage.getItem('refreshToken');
 
 // lets declare our 'engine'
 var appEngine = {
@@ -43,12 +50,12 @@ var appEngine = {
 		$('.page').css('height', height)
 
 		// show the loading screen
-		appEngine.showLoading()
+		appEngine.showLoading( messages.default_loading )
+		
 		
 	}, // end our init function
 
-	showLoading : function() {
-		console.log("showLoading")
+	showLoading : function(loading_msg) {
 		// position the spinner in the middle of the page
 		$('#loading .spinner').css('top', (height/2)-42+'px')
 		$('#loading h3').css('top', (height/2)-12+'px')
@@ -57,18 +64,27 @@ var appEngine = {
 		$('#loading').css('display', 'block')
 
 		// now lets show our Welcome page while we set everything else up
-		setTimeout(function() { 
-			$('#loading').css('display', 'none')
-			appEngine.showWelcome()
-		}, loadingOffset)
+		if( loading_msg() == messages.default_loading() ) {
+			// show the deafult message & then the home screen
+			$('.loading_msg').html( loading_msg() )
+			setTimeout(function() { 
+				$('#loading').css('display', 'none')
+				// right, we are done, show the welcome page
+				appEngine.showWelcome(null)
+			}, loadingOffset)
+		} else {
+			// show the message and wait, the engine will take care of the rest
+			$('.loading_msg').html( loading_msg() )
+		}
 
 	}, // end show the loading screen
 
-	showWelcome : function() {
+	showWelcome : function(message) {
 
 		// first up we need to check in localstorage to see if there are access tokens
 		if(accessToken) {
 			// if YES (there are access tokens) then move straight to getting the list of company files
+
 		} else {
 			// if NO (there are NO access tokens) then show the welcome screen
 
@@ -77,15 +93,23 @@ var appEngine = {
 				appEngine.secureMYOB()
 				return false // prevent the default browser action
 			})
-
+			// put the message in place
+			if(message) {
+				$('#welcome .lead_msg').html( message() )
+			} else {
+				// load the default
+				$('#welcome .lead_msg').html( messages.initial_welcome() )
+			}
 			$('#welcome').css('display', 'block')
 		} // end check for accessTokens
 		
 	}, // show Welcome
 
+	hideWelcome : function() {
+		$('#welcome').css('display', 'none')
+	}, // hide Welcome
+
 	secureMYOB : function() {
-		console.log("Child Browser");
-		var html = ''
 
 		// we are going to use a childBrowser so we can rip the code out of the URL 
 		window.plugins.childBrowser.showWebPage('https://secure.myob.com/oauth2/account/authorize?client_id='+theAPIkey+'&redirect_uri='+theAPIredirect_encoded+'&response_type=code&scope=CompanyFile', { showLocationBar: true });
@@ -95,24 +119,32 @@ var appEngine = {
 
 		    if( loc.indexOf(theAPIredirect + '?code') === 0 ){
 				//console.log('code found')
-				var code = loc.split('code=')
+				var code = loc.split('?code=')
 				//console.log(code[1])
-				html += '<br />Code: '+code[1]+'<br />Location: '+loc
-				
+				accessCode = code[1]
+				// close the childbrowser
+				window.plugins.childBrowser.close();
+
+				// now do the oauth token fetching
+				appEngine.showLoading( messages.oauth_token_fetching )
+				$('#loading').css('display', 'block')
+
+
+			} else if( loc.indexOf(theAPIredirect + '?error') === 0 ){
+				// there was an errror - handle it
+				//    error here is the user hit NO instead of YES
+				appEngine.showWelcome( messages.error_oauth_denied )
+				// close the childbrowser
 				window.plugins.childBrowser.close();
 
 			} else {
 				html += ('<br />code not found<br />')
 			}
-			/*
-		    if (loc.indexOf(serverUrl + '/?code') === 0) { 
-		        window.plugins.childBrowser.close(); 
-				$('.code').html(loc)
-		    } */
-
-			$('.code').html(html)
 		}
+
 	}, // secureMYOB
+
+
 } // end our engine
 
 
@@ -122,9 +154,35 @@ var appEngine = {
 //
 // document.ready used for testing in browser
 $(document).ready(function(){
-	appEngine.init()
+	appEngine.init( messages.default_loading )
 }) // end document ready
 
+
+//
+// Core messages 
+// Shouldn't really have HTML in the messages, nor should the messages really be in functions
+//   but hey, it's a sample and you are reading the comments ;)
+//
+
+var messages = {
+
+	initial_welcome : function() {
+		return 'To make full use of the awsesomeness of the cloud, bring together the power of MYOB AccountRight Live and our App of Awesome and watch business financial magic happen.';
+	}, 
+
+	error_oauth_denied : function() {
+		return '<div class="alert alert-error"><strong>Access Denied</strong><br />It seems you have declined access for this app to talk to your company files.<br />That is a shame, I\'m sad now.</div><p><strong>Want to try again?</strong> hit the button below to try again</p>'
+	},
+
+	default_loading : function() {
+		return 'Setting Up App'
+	},
+
+	oauth_token_fetching : function() {
+		return 'The Hamsters are off fetching data'
+	}
+
+} // end messages
 
 
 
