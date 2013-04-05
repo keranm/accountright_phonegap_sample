e3b24b320c2732d4233950c38f06a6ad762e9a18
Keran McKenzie <keran.mckenzie@gmail.com>
2013-04-05 23:00:31 +1100

    Changing the content type

--- js/main.js
+++ js/main.js
@@ -281,7 +281,7 @@ var appEngine = {
 		   $.ajax({
 				  type: "POST",
 				  url: oauthServer,
-				  contentType: 'application/x-www-form-urlencoded',
+				  contentType: 'multipart/form-data',
 				  data: theData,
 			    success: function(data) {
 			    	console.log("Refresh Token Received / Found? >> " + JSON.stringify(data));
